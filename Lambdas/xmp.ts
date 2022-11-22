4062b5b4a4ba51639bcdd1cd3c6c56d3a3985f2d
exlineo <contact@exlineo.com>
2022-11-22 10:14:37 +0100

    Ajout de la lecture des listes de données dans le XMP

--- Lambdas/xmp.ts
+++ Lambdas/xmp.ts
@@ -14,11 +14,13 @@ const s3 = new AWS.S3({ region: 'eu-west-3' });
 
 export const handler = async (event: any = {}): Promise<any> => {
 
-  const metas = []; // List of final metadata to send
-  const regXML = /<[^>]+\srdf\b[^>]*>/g; // Extract raw data from XMP
-  const regFiltre = /:(.*?)"(\n|>)/g; // Filter data in XMP
-  const schemas:Array<string> = []; // List of all data to get in medias
-  const prefix:any = {}; // Object to store prefix and there schemas
+  const metas:Array<any> = [];
+  const regXML = /<[^>]+\srdf\b[^>]*>/g; // Extraire les attributs de la balise rdf:Description
+  const regFiltre = /:(.*?)"(\n|>)/g; // Récupérer les meta dans la balise rdf:Description
+  const regTags = /<rdf:Description.*?>(.*?)<\/rdf:Description>/gms; // Extraire les balises incluses dans rdf:Description
+  const regLi = /<rdf:li.*?>(.*?)<\/rdf:li>/gms; // Récupérer les tableaux de données dans les balises
+  const prefix:any = {}; // Liste des prefix (namespaces) identifiés dans la base de données
+  const data:Array<any> = []; // Données en cours de traitement
 
   /** Get schema from a file in a bucket (deprecated) */
   const params = {
@@ -39,7 +41,7 @@ export const handler = async (event: any = {}): Promise<any> => {
   }
   
   /** End script if no body was send. Body is used as prefix to scan a specific folder */
-  const liste = await s3.listObjectsV2({ "Bucket": BUCKET, "Prefix": event.body }).promise();
+  const liste:any = await s3.listObjectsV2({ "Bucket": BUCKET, "Prefix": event.body }).promise();
   if (!event.body) {
     const dir: Array<string> = [];
     for (let i = 0; i < liste.Contents!.length; ++i) {
@@ -50,33 +52,65 @@ export const handler = async (event: any = {}): Promise<any> => {
   } else {
     // Get data from objects
     for (let i = 0; i < liste.Contents!.length; ++i) {
-      const data: Array<string> = [];
-      const obj: any = {};
+      let { Body } = await s3.getObject({ "Bucket": BUCKET, "Key": liste.Contents[i].Key }).promise();
+      
       let exe;
-      // Get data from a specific object (selected by Key)
-      // let { Body } = await s3.getObject({ "Bucket": BUCKET, "Key": liste.Contents![i].Key!, "Range": "bytes=0-4096" }).promise();
-      let { Body } = await s3.getObject({ "Bucket": BUCKET, "Key": liste.Contents![i].Key! }).promise();
+      const obj:any = {};
+      // console.log(Body.toString());
       let meta = regXML.exec(Body!.toString());
-      regXML.lastIndex = 0; // Réinitialisation de l'index du regex pour pour scanner tous les objet (ne scan qu'un sur deux sinon)
-      if (meta) {
-        // Extract data from raw file
-        while (exe = regFiltre.exec(meta[0])) {
-          data.push(exe[1]);
+        if (meta) {
+          // console.log("Meta : " + meta);
+          while ((exe = regFiltre.exec(meta[0]))) {
+            data.push(exe[1]);
+          }
+          
+          // Récupérer les variables dans les attributs de la balise rdf:Description
+          for (let j = 0; j < data.length; ++j) {
+            let tmp = data[j].split('="');
+            for (let p in prefix) {
+              if (!obj[p])
+                obj[p] = {};
+              if (prefix[p].schema.values.includes(tmp[0])) {
+                obj[p][tmp[0]] = tmp[1];
+              }
+            };
+          }
         }
-        // List data to extract metadata from the namespaces
-        for (let j = 0; j < data.length; ++j) {
-          let tmp = data[j].split('="');
-          for(let j in prefix){
-            if(!obj[j]) obj[j] = {};
-            if (prefix[j].schema.values.includes(tmp[0])) {
-              obj[j][tmp[0]] = tmp[1];
+      
+      // let tags = regTags.exec(Body.toString());
+      let tags = Body!.toString().match(regTags);
+      console.log("Tags : " + Body!.toString().match(regTags));
+      
+      if(tags){
+        let tt;
+        // Traitement des balise rdf:li
+        for (let p in prefix) {
+          const pre = prefix[p];
+          for(let s = 0; s < pre.schema.values.length; ++s){
+            const schem = pre.schema.values[s];
+            // console.log("id schema > " + schem);
+            const re = new RegExp(`<${pre.id}:${schem}>(.+)\<\/${pre.id}:${schem}>`, 'gms');
+            // console.log(re + ' > matches : ' + re.exec(tags[0]));
+            const tmpTags:any = re.exec(tags[0]);
+            if(tmpTags){
+              obj[p][schem] = [];
+              while ((tt = regLi.exec(tmpTags)))
+              {
+                if(!obj[p][schem].includes(tt[1])) obj[p][schem].push(tt[1]);
+              };
+              // Si le tableau n'a qu'un valeur, on en fait une chaîne de caractère
+              if(obj[p][schem].length < 2) obj[p][schem] = obj[p][schem].join();
             }
           }
         }
-        metas.push(obj);
       }
-    };
-    console.log(metas);
+      
+      regXML.lastIndex = 0;
+      regTags.lastIndex = 0;
+      
+      metas.push(obj);
+    }
+    // console.log(metas);
     return { statusCode: 200, body: metas };
   }
 }
